baf0fbe884186adf37ec8c3ce92d76d98cae9236
Wesley <cptwesley@jojoweba.nl>
2017-11-01 19:09:06 +0100

    Add annotation parameters

--- src/main/java/nl/cptwesley/whitebox/WhiteBox.java
+++ src/main/java/nl/cptwesley/whitebox/WhiteBox.java
@@ -11,4 +11,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.TYPE)
 public @interface WhiteBox {
+    String target();
+    WhiteBoxDepth depth() default WhiteBoxDepth.Public;
 }
---
+++ src/main/java/nl/cptwesley/whitebox/WhiteBoxDepth.java
@@ -0,0 +1,8 @@
+package nl.cptwesley.whitebox;
+
+/**
+ * Identifiers for how deep we should grant access to a black box class.
+ */
+public enum WhiteBoxDepth {
+    Public, Protected, Package, Private
+}
